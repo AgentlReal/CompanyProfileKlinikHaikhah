60b61eeebb75170c3d9f61f33177bc1be6fd7dcb
AgentlReal <luqmanhaikhah68@gmail.com>
2025-04-26 11:26:15 +0700

    menambahkan fungsionalitas homepage dengan menambahkan global.js dan index.js

---
+++ resources/js/global.js
@@ -0,0 +1,30 @@
+//Toggle Navbar
+function toggleNavBar(e) {
+  document.querySelector(".nav-links").classList.toggle("show");
+}
+
+document.querySelectorAll(".nav-icon").forEach(function (element) {
+  element.addEventListener("click", (e) => toggleNavBar(e));
+});
+
+//Entering  Animation
+const animatedOnScrollElements = document.querySelectorAll(".aos");
+
+function runAnimationOnSroll() {
+  animatedOnScrollElements.forEach(function (element) {
+    if (element.getBoundingClientRect().top < window.innerHeight * 0.9) {
+      element.classList.add("aos-on");
+    }
+  });
+}
+
+runAnimationOnSroll();
+window.addEventListener("scroll", () => runAnimationOnSroll());
+
+//Clear Service Card Entering Animation
+const serviceCards = document.querySelectorAll(".service-card");
+serviceCards.forEach(function (element) {
+  element.addEventListener("mouseenter", function (e) {
+    element.classList.remove("aos");
+  });
+});
